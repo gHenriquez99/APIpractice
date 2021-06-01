6af1eafec5fcf9e5def3ae7ed5daa7d246af4ae3
gHenriquez99 <henriquez.gus99@gmail.com>
2021-06-01 17:14:20 -0400

    adds index.html inside of public folder

--- index.js
+++ index.js
@@ -2,22 +2,14 @@ import express from 'express';
 const app = express();
 import apiRoutes from './apiRoutes.js';
 
-// this is not working. Tried to set up a router from the things.js file
-// import * as things from './things.js';
-app.get('/', function(req, res){
-    res.send("Hello, Emma");
-});
+const staticFolder = 'public';
 
-app.post('/', function(req, res){
-    res.send("This calls a post method at '/'");
-});
+// all my routes are in a Router in apiRoutes.js to help clean up this page
 
-app.all('/test', function(req, res){
-    res.send("This is for all HTTP methods. This is commonly used for defining middleware.");
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(staticFolder));
 
-// part of the router that is not working
-//app.use('/things', things);
 app.use('/api', apiRoutes);
 
 app.listen(3000);
